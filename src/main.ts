3391a23fa6afe32c07b76c1dc99a3ea0796515d6
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2022-04-12 01:11:50 +0500

    update cookie

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('etag', 'strong');
 
   app.enableCors({
     credentials: true,
@@ -18,6 +21,7 @@ async function bootstrap() {
   app.use(
     cookieSession({
       keys: ['user'],
+      'same-site': 'none',
     }),
   );
   await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
