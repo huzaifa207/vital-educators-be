97f766430cc1301a4a3e35dfe5d96d8efde0dd00
Muhammad Husnain <mlhlk1212@gmail.com>
2022-07-20 16:24:39 +0500

    update: subject offer

--- src/tutors/subject-offers/subject-offers.service.ts
+++ src/tutors/subject-offers/subject-offers.service.ts
@@ -60,6 +60,7 @@ export class SubjectOffersService {
           },
           data: {
             ...updateSubjectOfferDto,
+            title: updateSubjectOfferDto.title.toString().toLowerCase(),
           },
         });
       }
