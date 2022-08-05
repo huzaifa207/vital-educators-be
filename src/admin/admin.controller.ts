a006346f0094b333987bdea027f8949bbe747154
Humayun Javed <arnetraff@gmail.com>
2022-08-05 15:27:32 +0500

    feat: referees in admin/tutor/:id response

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,13 +1,22 @@
 import { Controller, DefaultValuePipe, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { RefereesService } from 'src/tutors/referees/referees.service';
 import { TutorsService } from 'src/tutors/tutors.service';
 
 @Controller('/admin')
 export class AdminController {
-  constructor(private readonly tutorService: TutorsService) {}
+  constructor(
+    private readonly tutorService: TutorsService,
+    private readonly refereeService: RefereesService,
+  ) {}
   @Get('tutor/:tutorId')
-  getTutorById(@Param('tutorId', new ParseIntPipe()) tutorId: number) {
-    return this.tutorService.getTutorProfile(tutorId, {
+  async getTutorById(@Param('tutorId', new ParseIntPipe()) tutorId: number) {
+    const tutorDetails = await this.tutorService.getTutorProfile(tutorId, {
       userExcludedFields: ['password', 'password_reset_token'],
     });
+
+    return {
+      ...tutorDetails,
+      referees: await this.refereeService.findAll(tutorId),
+    };
   }
 }
--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { RefereesModule } from 'src/tutors/referees/referees.module';
+import { RefereesService } from 'src/tutors/referees/referees.service';
 import { TutorsService } from 'src/tutors/tutors.service';
 import { AdminController } from './admin.controller';
 
 @Module({
-  imports: [],
+  imports: [RefereesModule],
   controllers: [AdminController],
   providers: [PrismaService, TutorsService],
 })
