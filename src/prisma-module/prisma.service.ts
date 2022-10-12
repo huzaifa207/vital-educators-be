736fd8104035c93f2d55c8ab5edf198d342dbb53
Humayun Javed <arnetraff@gmail.com>
2022-10-12 21:19:43 +0500

    fix: change max con to 15

--- src/prisma-module/prisma.service.ts
+++ src/prisma-module/prisma.service.ts
@@ -6,7 +6,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     console.log('DB:', process.env.DATABASE_URL);
     super({
-      datasources: { db: { url: process.env.DATABASE_URL + '?connection_limit=1' } },
+      datasources: { db: { url: process.env.DATABASE_URL + '?connection_limit=15' } },
     });
   }
   async onModuleInit() {
