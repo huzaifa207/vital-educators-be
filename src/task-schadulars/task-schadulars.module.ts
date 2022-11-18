be3c4808fe471cbad53c4cb12ad29016f542a895
Humayun Javed <humayunjaved23@gmail.com>
2022-11-18 22:38:14 +0500

    fix

--- src/task-schadulars/task-schadulars.module.ts
+++ src/task-schadulars/task-schadulars.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MailService } from 'src/mail-service/mail.service';
-import { TutorsModule } from 'src/tutors/tutors.module';
 import { TutorsService } from 'src/tutors/tutors.service';
 import { TaskSchadularsService } from './task-schadulars.service';
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), ScheduleModule.forRoot(), TutorsModule],
+  imports: [EventEmitterModule.forRoot(), ScheduleModule.forRoot()],
   controllers: [],
-  providers: [TaskSchadularsService, MailService, TutorsService],
+  providers: [TaskSchadularsService, TutorsService, MailService],
   exports: [TaskSchadularsService],
 })
 export class TaskSchadularsModule {}
